ccefc039e33e6eb33bf64c836c41bd3eb9b3ef84
Ben Kimock <kimockb@gmail.com>
2026-07-05 17:53:56 +0000

    Checkpoint

---
+++ interesting.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eu
+
+cargo +nightly -Zscript $REPRODUCER 2>&1 | grep 'panicked at /rustc-dev/c397dae808f70caebab1fc4e11b3edf7e59f58c7/compiler/rustc_middle/src/ty/instance.rs:616:21'
